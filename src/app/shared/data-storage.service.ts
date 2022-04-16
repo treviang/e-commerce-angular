5c4115cad60bab3e95430c2982c1480c593aabf6
Angelo Trevisiol <angelo.trevisiol@gmail.com>
2022-04-16 12:03:51 +0200

    introdotto resolver

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
@@ -21,14 +21,14 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http.get<Recipe[]>('https://ng-course-ecommerce-20938-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+        return this.http.get<Recipe[]>('https://ng-course-ecommerce-20938-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
             .pipe(map(recipes => {
                 return recipes.map(recipe => {
                     return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
                 })
-            }))
-            .subscribe(response => {
-                this.recipeService.setRecipes(response);
-            });
+            }),
+            tap(recipes => {
+                this.recipeService.setRecipes(recipes);
+            } ));
     }
 }
